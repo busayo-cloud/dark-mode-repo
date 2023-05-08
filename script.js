d5bf98eaf7d9942f5c1fe8f660fb2ea1be34c976
busayo <busayostowe1@gmail.com>
2023-05-07 21:36:53 -0700

    busayo

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const innerEl = document.querySelector('.input')
-console.log(innerEl.checked)
+const hourEl = document.getElementById('hour')
+const minuteEl = document.getElementById('minute')
+const secondEl = document.getElementById('seconds')
+const ampmEl = document.getElementById('ampm')
 
-const bodyEl = document.querySelector('body')
-console.log(bodyEl.checked)
+function updateClock() {
+  let h = new Date().getHours()
+  let m = new Date().getMinutes()
+  let s = new Date().getSeconds()
+  let ampm = 'PM'
 
-innerEl.checked = JSON.parse(localStorage.getItem('mode'))
-
-function myFunction() {
-  if (innerEl.checked) {
-    bodyEl.style.backgroundColor = 'black'
-  } else {
-    bodyEl.style.backgroundColor = 'white'
+  if (h < 12) {
+    h = h - 12
+    ampm = 'AM'
   }
+  h = h < 10 ? '0' + h : h
+  m = m < 10 ? '0' + m : m
+  s = s < 10 ? '0' + s : s
+  hourEl.innerText = h
+  minuteEl.innerText = m
+  secondEl.innerText = s
+  ampmEl.innerText = ampm
+  setTimeout(() => {
+    updateClock()
+  }, 1000)
 }
-myFunction()
-innerEl.addEventListener('input', () => {
-  myFunction()
-  localStorage()
-})
-
-function LocalStorage() {
-  localStorage.setItem('mode', JSON.stringify(innerEl.checked))
-}
+updateClock()
